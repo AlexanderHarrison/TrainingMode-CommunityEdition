1070797ed769d76554cb9248be3038e5b13f541d
Alex Harrison <alexander.harrison.r@gmail.com>
2025-07-20 18:26:50 -0600

    add build script and remove makefile

---
+++ build.sh
@@ -0,0 +1,108 @@
+#! /usr/bin/env bash
+
+set -Eeo pipefail
+
+# ensure an iso was passed
+if [ -z "${1}" ]; then
+    echo "Usage:"
+    echo "    $0 <path/to/vanilla/melee.iso> [release]"
+    exit 1
+fi
+iso="${1}"
+build="${2}"
+
+if [ ! -f "${iso}" ]; then
+    echo "Error: path '${iso}' does not exist!"
+    exit 1
+fi
+
+# check if en or jp and use appropriate patch
+HEADER=$(bin/gc_fst get-header "${iso}")
+if [ "${HEADER}" = "GALE01" ]; then
+    PATCH="patch.xdelta"
+elif [ "${HEADER}" = "GALJ01" ]; then
+    PATCH="patch_jp.xdelta"
+else
+    echo "Error: Invalid ISO '${iso}'"
+    exit 1
+fi
+
+# fn to kill all processes everywhere
+kill_all() {
+    trap "kill -- -$$" EXIT
+}
+
+# fn to build mex executable
+mex_build() {
+    local mode="${1}"
+    local out="${2}"
+    local src="${3}"
+    
+    if [ -n "${4}" ]; then
+        local dat="-dat ${4}"
+    else
+        local dat=""
+    fi
+    
+    if [ "${build}" = "release" ]; then
+        local opt="-O2"
+    else
+        local opt=""
+    fi
+    
+    bin/hmex -q -l "MexTK/melee.link" -f "-w -fpermissive ${opt}" -s "${mode}" -t "MexTK/${mode}.txt" -o "${out}" -i ${src} ${dat} || kill_all
+}
+
+# make build directory if necessary
+mkdir -p build
+
+# compile asm
+bin/hgecko -q ASM build/codes.gct
+
+# compile code in parallel
+mex_build "tmFunction" "build/eventMenu.dat" "src/events.c src/menu.c src/savestate_v1.c" "dats/eventMenu.dat" &
+mex_build "cssFunction" "build/labCSS.dat" "src/lab_css.c" "dats/labCSS.dat" &
+mex_build "evFunction" "build/lab.dat" "src/lab.c" "dats/lab.dat" &
+mex_build "evFunction" "build/lcancel.dat" "src/lcancel.c" "dats/lcancel.dat" &
+mex_build "evFunction" "build/ledgedash.dat" "src/ledgedash.c" "dats/ledgedash.dat" &
+mex_build "evFunction" "build/wavedash.dat" "src/wavedash.c" "dats/wavedash.dat" &
+mex_build "evFunction" "build/powershield.dat" "src/powershield.c" &
+mex_build "evFunction" "build/edgeguard.dat" "src/edgeguard.c" &
+
+# build mex Start.dol
+bin/gc_fst read "${iso}" Start.dol build/ISOStart.dol
+xdelta3 -dfs build/ISOStart.dol "Build TM Start.dol/${PATCH}" build/Start.dol
+
+# copy iso over
+if [ ! -f TM-CE.iso ]; then cp "${iso}" TM-CE.iso; fi
+
+# wait for compilation to finish
+wait
+
+# add TM files to iso
+bin/gc_fst fs TM-CE.iso \
+    delete MvHowto.mth \
+    delete MvOmake15.mth \
+    delete MvOpen.mth \
+    insert TM/eventMenu.dat build/eventMenu.dat \
+    insert TM/lab.dat build/lab.dat \
+    insert TM/labCSS.dat build/labCSS.dat \
+    insert TM/lcancel.dat build/lcancel.dat \
+    insert TM/ledgedash.dat build/ledgedash.dat \
+    insert TM/wavedash.dat build/wavedash.dat \
+    insert TM/powershield.dat build/powershield.dat \
+    insert TM/edgeguard.dat build/edgeguard.dat \
+    insert codes.gct build/codes.gct \
+    insert Start.dol build/Start.dol \
+    insert opening.bnr opening.bnr
+bin/gc_fst set-header TM-CE.iso "GTME01" "Training Mode Community Edition"
+
+echo "Successfully built TM-CE.iso"
+
+# build release
+if [ "${2}" = "release" ]; then
+    xdelta3 -fs "${iso}" -e "TM-CE.iso" "TM-CE/patch.xdelta"
+    zip -r TM-CE.zip TM-CE/
+    echo "Successfully built TM-CE.zip"
+fi
+
