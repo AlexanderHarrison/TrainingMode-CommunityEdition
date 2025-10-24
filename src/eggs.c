ca3dac136fbd4224a1dabd524a240374a0367d16
jams <james.ge1@outlook.com>
2025-10-24 07:56:28 -0700

    handle egg gfx properly, score UI, handle high scores, more menu options

--- src/eggs.c
+++ src/eggs.c
@@ -2,28 +2,125 @@
 #include "events.h"
 
 void StartFreePractice(GOBJ *gobj);
+GOBJ *Egg_Spawn(void);
 
-void Exit(GOBJ *menu) {
+void *(*Events_SetEventAsPlayed)(int event_id) = (void *(*)(int event_id)) 0x8015ceb4;
+void *(*Events_StoreEventScore)(int event_id, int score) = (void *(*)(int event_id, int score)) 0x8015cf70;
+int (*Events_GetSavedScore)(int event_id) = (int (*)(int event_id)) 0x8015cf5c;
+void *(*MatchInfo_0x0010_store)(int unk) = (void *(*)(int unk)) 0x8016b350;
+void *(*Egg_Destroy)(GOBJ *egg_gobj) = (void *(*)(GOBJ *egg_gobj)) 0x80289158;
+void *(*Event_Retry)(void) = (void *(*)(void)) 0x8016cf4c;
+
+static int egg_counter = 0;
+static int high_score = 0;
+Vec3 coll_pos, last_coll_pos;
+GOBJ *egg_gobj, *effect_gobj;
+JOBJ *effect_jobj;
+CmSubject *cam;
+
+GOBJ *hud_score_gobj, *hud_best_gobj;
+JOBJ *hud_score_jobj, *hud_best_jobj;
+static int canvas;
+Text *hud_score_text, *hud_best_text;
+
+void Exit(GOBJ *menu) 
+{
     stc_match->state = 3;
     Match_EndVS();
 }
 
+void Retry(GOBJ *menu) 
+{
+    stc_match->end_kind = MATCHENDKIND_RETRY;
+    stc_match->state = 3;
+    Match_EndVS();
+}
+
+static void ChangeEggSize(GOBJ *menu, int value)
+{
+    if (egg_gobj != 0)
+    {
+        Item_Destroy(egg_gobj);
+        egg_gobj = Egg_Spawn();
+    }
+}
+
+void ChangeHitDisplay(GOBJ *menu_gobj, int value)
+{
+    // loop through all fighters
+    GOBJ *this_fighter = (*stc_gobj_lookup)[MATCHPLINK_FIGHTER];
+    while (this_fighter != 0)
+    {
+        // get data
+        FighterData *thisFighterData = this_fighter->userdata;
+
+        // toggle
+        thisFighterData->show_hit = value;
+
+        // get next fighter
+        this_fighter = this_fighter->next;
+    }
+}
+
 enum options_main 
 {
+    OPT_RETRY,
     OPT_FREEPRACTICE,
+    OPT_DAMAGETHRESHOLD,
+    OPT_SCALE,
+    OPT_COLLISION,
     OPT_EXIT,
 
     OPT_COUNT
 };
 
+static float EggOptions_Size[] = {1.0f, .5f, 2.0f};
+static const char *EggOptions_SizeText[] = {"Normal", "Small", "Large"};
 static EventOption Options_Main[OPT_COUNT] = {
     {
         .kind = OPTKIND_FUNC,
-        .name = "Start Free Practice",
-        .desc = { "Enable endless practice." },
+        .name = "Retry",
+        .desc = { "Retry this event. Pressing Z while paused also does this." },
+        .val = 0,
+        .disable = 0,
+        .OnSelect = Retry
+    },
+    {
+        .kind = OPTKIND_FUNC,
+        .name = "Enable free practice",
+        .desc = { "Start Free Practice mode, giving infinite time and enabling\nother options." },
         .val = 0,
         .disable = 0,
         .OnSelect = StartFreePractice
+    },
+    {
+        .kind = OPTKIND_INT,
+        .name = "Damage Threshold",
+        .desc = { "Adjust the minimum damage needed to break an egg." },
+        .val = 12,
+        .format = "%d",
+        .value_min = 0,
+        .value_num = 200,
+        .disable = 1
+    },
+    {
+        .kind = OPTKIND_STRING,
+        .value_num = sizeof(EggOptions_SizeText) / 
+                     sizeof(*EggOptions_SizeText),
+        .name = "Egg scale",
+        .desc = { "Adjust egg size." },
+        .values = EggOptions_SizeText,
+        .disable = 1,
+        .OnChange = ChangeEggSize
+    },
+    {
+        .kind = OPTKIND_TOGGLE,
+        .name = "Fighter Collision",
+        .desc = {"Toggle hitbox and hurtbox visualization.",
+                 "Hurtboxes: yellow=hurt, purple=ungrabbable, blue=shield.",
+                 "Hitboxes: (by priority) red, green, blue, purple."},
+        .disable = 1,
+        .OnChange = ChangeHitDisplay,
     },
     {
         .kind = OPTKIND_FUNC,
@@ -35,6 +132,9 @@ static EventOption Options_Main[OPT_COUNT] = {
 
 void StartFreePractice(GOBJ *gobj) {
     Options_Main[OPT_FREEPRACTICE].disable = 1;
+    Options_Main[OPT_DAMAGETHRESHOLD].disable = 0;
+    Options_Main[OPT_SCALE].disable = 0;
+    Options_Main[OPT_COLLISION].disable = 0;
     stc_match->match.timer = MATCH_TIMER_COUNTUP;
 }
 
@@ -44,24 +144,59 @@ static EventMenu Menu_Main = {
     .options = Options_Main,
 };
 
-static int egg_counter = 0;
-Vec3 coll_pos, last_coll_pos;
-GOBJ *egg_gobj, *effect_gobj;
-JOBJ *effect_jobj;
-CmSubject *cam;
+
 
 void Event_Init(GOBJ *gobj)
 {
     // initialize egg camera subject
     cam = CameraSubject_Alloc();
-    cam->boundleft_proj = -5;
+    cam->boundleft_proj = -10;
     cam->boundright_proj = 10;
     cam->boundtop_proj = 10;
     cam->boundbottom_proj = -10;
 
     // gobj for writing gfx to 
-    effect_gobj = GObj_Create(10, 11, 0);
+    effect_gobj = GObj_Create(0, 0, 0);
     effect_jobj = effect_gobj->hsd_object;
+
+    // hud setup
+    hud_score_gobj = GObj_Create(0, 0, 0);
+    GObj_AddObject(hud_score_gobj, 3, hud_score_jobj);
+    GObj_AddGXLink(hud_score_gobj, GXLink_Common, GXLINK_HUD, 80);
+
+    hud_best_gobj = GObj_Create(0, 0, 0);
+    GObj_AddObject(hud_best_gobj, 3, hud_best_jobj);
+    GObj_AddGXLink(hud_best_gobj, GXLink_Common, GXLINK_HUD, 80);
+    canvas = Text_CreateCanvas(2, 0, 0, 0, 0, GXLINK_HUD, 81, 19);
+    
+    // Create text objects
+    hud_score_text = Text_CreateText(2, canvas);
+    hud_score_text->kerning = 1;
+    hud_score_text->align = 0;
+    hud_score_text->use_aspect = 1;
+    hud_score_text->trans.X = 50;
+    hud_score_text->trans.Y = 25;
+    hud_score_text->viewport_scale.X = 1;
+    hud_score_text->viewport_scale.Y = 1;
+    
+    hud_best_text = Text_CreateText(2, canvas);
+    hud_best_text->kerning = 1;
+    hud_best_text->align = 0;
+    hud_best_text->use_aspect = 1;
+    hud_best_text->trans.X = 50;
+    hud_best_text->trans.Y = 40;
+    hud_best_text->scale.X = 0.35;
+    hud_best_text->scale.Y = 0.35;
+    hud_best_text->viewport_scale.X = 1;
+    hud_best_text->viewport_scale.Y = 1;
+
+    // dummy text
+    Text_AddSubtext(hud_score_text, 0, 0, "-");
+    Text_AddSubtext(hud_best_text, 0, 0, "-");
+    
+    high_score = Events_GetSavedScore(stc_memcard->EventBackup.event);
+
+    stc_match->end_kind = MATCHENDKIND_NONE;
 }
 
 // generate random float between low and high
@@ -119,7 +254,7 @@ GOBJ *Egg_Spawn(void)
     }
     
     // random Y velocity on spawn
-    Vec3 rand_velocity = {0, 0.4 + HSD_Randf() * 2, 0};
+    Vec3 rand_velocity = {0, RandomRange(0, 2), 0};
     SpawnItem item_egg = {
         .it_kind = ITEM_EGG,
         .pos = coll_pos,
@@ -137,18 +272,16 @@ int Egg_OnTakeDamage(GOBJ *gobj)
 {
     // gfx and sfx
     ItemData *egg_data = egg_gobj->userdata;
-    effect_jobj->trans = egg_data->pos;
-    Effect_SpawnSync(1232, effect_gobj, &effect_jobj->trans);
-    Item_PlayOnDestroySFXAgain(egg_data, 244, 127, 64);
-
-    // manage old and new egg
-    Item_Destroy(gobj);
-    egg_gobj = Egg_Spawn();
-    egg_counter++;
-
-    char buffer[10];
-    sprintf(buffer, "%d\n", egg_counter);
-    OSReport(buffer);
+    int damage = egg_data->dmg.recent;
+    if (damage >= Options_Main[OPT_DAMAGETHRESHOLD].val){
+        Effect_SpawnSync(1232, gobj, egg_data->pos);
+        Item_PlayOnDestroySFXAgain(egg_data, 244, 127, 64);
+        
+        // manage old and new egg
+        Egg_Destroy(gobj);
+        egg_gobj = Egg_Spawn();
+        egg_counter++;
+    }
     return 0;
 }
 
@@ -185,13 +318,65 @@ void Event_Think(GOBJ *event)
         egg_data->can_nudge = 0;
         egg_data->self_vel.X = 0;
 
-        // set these callbacks every frame or else gets overwritten
+        // apply scaling changes
+        JOBJ *egg_jobj = Item_GetBoneJOBJ(egg_gobj, 0);
+        egg_jobj->scale.X = EggOptions_Size[Options_Main[OPT_SCALE].val];
+        egg_jobj->scale.Y = EggOptions_Size[Options_Main[OPT_SCALE].val];
+        egg_jobj->scale.Z = EggOptions_Size[Options_Main[OPT_SCALE].val];
+        egg_data->scale = EggOptions_Size[Options_Main[OPT_SCALE].val];
+
+        // show egg hurtbox on hit
+        egg_data->show_hit = Options_Main[OPT_COLLISION].val;
+
+        // set this callbacks every frame or else gets overwritten
         egg_data->it_func->OnTakeDamage = Egg_OnTakeDamage;
 
         // update camera position
         cam->cam_pos.X = egg_data->pos.X;
         cam->cam_pos.Y = egg_data->pos.Y + 15; // add so it's not awkwardly low
         cam->cam_pos.Z = egg_data->pos.Z;
+    }
+
+    // Update egg count, hide when paused
+    if (Pause_CheckStatus(1) != 2)
+    {
+        Text_SetText(hud_score_text, 0, "%d", egg_counter);
+        if (high_score > 0)
+        {
+            Text_SetText(hud_best_text, 0, "Best: %d", high_score);
+        }
+        else
+        {
+            Text_SetText(hud_best_text, 0, "");
+        }
+    } 
+    else
+    {
+        Text_SetText(hud_score_text, 0, "");
+        Text_SetText(hud_best_text, 0, "");
+    }
+
+    // After 1 minute and not in free practice
+    if (stc_match->time_frames == 360 && stc_match->match.timer != MATCH_TIMER_COUNTUP)
+    {   
+        int event_id = stc_memcard->EventBackup.event;
+        Events_SetEventAsPlayed(event_id); // I don't know why this doesn't work, but it really should
+        Match_SetEndGraphic(MATCHENDKIND_VSGAME);
+
+        // high score logic
+        int high_score = Events_GetSavedScore(event_id);
+        if (egg_counter > high_score)
+        {
+            MatchInfo_0x0010_store(0x9c40); // Makes "new record" audio happen, IDK why
+            Match_SetPostMatchSFX(325);
+            Events_StoreEventScore(event_id, egg_counter);
+        }
+        else
+        {
+            Match_SetPostMatchSFX(324);
+        }
+
+        Match_EndImmediate();
     }
 }
 
